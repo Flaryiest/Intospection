066bdfab213f62cd15b011ddf178b27e11641e9d
Flaryiest <ericmzuo1@gmail.com>
2026-03-13 13:11:54 -0600

    navbar and homepage structure

--- web/src/components/navbar/navbar.tsx
+++ web/src/components/navbar/navbar.tsx
@@ -1,5 +1,23 @@
+import styles from "./navbar.module.css"
+import { Link } from "react-router-dom"
 export default function Navbar() {
-    return <div>
-        
-    </div>
+    return <header className={styles.header}>
+        <h3 className={styles.logo}>Eric Zuo</h3>
+        <nav className={styles.nav}>
+            <ul className={styles.navList}>
+                <li className={styles.navItem}>
+                    <Link to="/">About</Link>
+                </li>
+                <li className={styles.navItem}>
+                    <Link to="/about">Experiences</Link>
+                </li>
+                <li className={styles.navItem}>
+                    <Link to="/about">Writing</Link>
+                </li>
+                <li className={styles.navItem}>
+                    <Link to="/about">Artifacts</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
 }
--- web/src/pages/home/home.tsx
+++ web/src/pages/home/home.tsx
@@ -3,6 +3,19 @@ import Navbar from "@components/navbar/navbar.tsx";
 export function Home() {
     return <div className={styles.page}>
         <Navbar />
-        <h1>Home</h1>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi! I'm Eric.</h1>
+            <h2 className={styles.subtitle}>17 y/o Chinese Canada living in Canada. Currently sidequesting and exploring the world, one step at a time. Most recently I have been working at a few startups, including Magic Hour (YC W24) and Hamming AI (YC S24).</h2>
+            <div className={styles.introText}>More about me and my work can found be found here.</div>
+            <div className={styles.sectionDivider}></div>
+            <div className={styles.sectionTwo}>
+                <h2 className={styles.sectionTwoTitle}>In order to be human: create</h2>
+                <div className={styles.sectionTwoText}>
+                    <p>In my free time, I enjoy writing and reading all types of works.</p>
+                    <p>Will hopefully be sharing most of my writing here in the future, and would always love to get thoughts!</p>
+                </div>  
+            </div>
+            <div className={styles.sectionDivider}></div>
+        </div>
     </div>
 }
